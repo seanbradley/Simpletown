e94cc55cd2344f4bb55ea39391527ffc925184ae
Sean Bradley <sean@bravoflix.com>
2013-07-01 02:15:14 -0700

    Change home page to index.html.

--- sb_biz/urls.py
+++ sb_biz/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'sb_biz.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    #url(r'^$', 'sb_biz.views.home', name='home'),
     # url(r'^sb_biz/', include('sb_biz.foo.urls')),
 
+    url(r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
