93b9e25182ee2ea51e2e7178063f332c892f2d05
seanbradley <sean@blogblimp.com>
2013-07-06 06:25:22 +0000

    Incl. registration app in urls.py.

--- urls.py
+++ urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from sb_biz.views import IndexView
-from cities.views import ViewOne, ViewTwo
+from cities import views
+
+#from cities.views import view_one #, view_two #ViewOne, ViewTwo
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
 
     url(r'^$', IndexView.as_view()),
-    url(r'^view_one/', ViewOne.as_view()),    
-    url(r'^view_two/', ViewTwo.as_view()),
+    url(r'^view_one', 'cities.views.view_one'),
         
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+  
+    #url(r'^view_one/', ViewOne.as_view()),    
+    #url(r'^view_two/', ViewTwo.as_view()),
     
 )
