c0a088452e611c3f53bd3531c77fb1682378b094
Sean Bradley <sean@bravoflix.com>
2013-07-07 04:05:06 -0700

    Clean up base url conf.

--- urls.py
+++ urls.py
@@ -1,30 +1,24 @@
 from django.conf.urls import patterns, include, url
-#from django.views.generic.simple import direct_to_template
 from django.contrib import admin
-from sb_biz import views
-from cities import views
 admin.autodiscover()
+
+import registration
+from sb_biz.views import IndexView
+from cities import views
 
 
 urlpatterns = patterns('',
 
-    ###### home page #####
-    #url(r'^$', direct_to_template, { 'template': 'index.html' }, 'index'),
+    ###### index #####
     url(r'^$', IndexView.as_view()),
     
-    ##### view one #####
+    ##### cities #####
     url(r'^view_one', 'cities.views.view_one'),
 
-    ##### login, logout, etc #####
+    ##### registration #####
     url(r'^accounts/', include('registration.urls')),
-    #url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 
-    ##### admin dashboard #####
+    ##### admin #####
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
-    ##### urls for CBVs #####
-    #url(r'^view_one/', ViewOne.as_view()),    
-    #url(r'^view_two/', ViewTwo.as_view()),
     
 )
