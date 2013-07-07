7f72355242b45d505e64e784247c5d840225aff5
Sean Bradley <sean@bravoflix.com>
2013-07-07 03:57:29 -0700

    Setting up views and templates for view_two.

--- cities/views.py
+++ cities/views.py
@@ -1,22 +1,17 @@
 import re, requests, json
-from itertools import compress
-#from operator import itemgetter, attrgetter
-
-from django.views.generic.base import View, TemplateView, TemplateResponse
-from django.template import loader, RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
 from cities.models import City
   
                       
-def filter_one(messyData):
+def filter_one(data):
     
-    for x in messyData:
+    for x in data:
         
         del x["description"], x["feat_class"], x["feature_id"], x["fips_class"], x["fips_county_cd"], x["full_county_name"], x["link_title"], x["url"], x["state_abbreviation"]
                         
-    return messyData
+    return data
     
 
 @login_required
@@ -46,22 +41,23 @@ def view_one(self):
         # append our geo_data list with one state
         geo_data.append(clean_data)
         
-    # return all states...and mop up some bits and pieces of ancillary jive
+    # return all states...and mop up some bits and pieces of ancillary jsonic jive
     all_geo_data = str(geo_data).replace(',', '\n').replace('u\'', '\'').replace('\'', '').replace('[', '').replace(']', '').replace('{', '').replace('}', '\n')
         
     context = { 'all_geo_data': all_geo_data }
     
     return render_to_response('cities/view_one.html', context)
     
-'''
-class ViewTwo(TemplateView):
- 
-    template_name = "cities/view_two.html"
 
-    #list_by_county = []
-    
-    def get_context_data(self):
-        return {
-            #'list_by_county': list_by_county
-        }
+'''
+# See http://pydanny.com/the-easy-form-views-pattern-controversy.html
+# vs. http://pydanny.com/core-concepts-django-modelforms.html
+@login_required+
+def view_two(request, template_name="cities/cities_form.html"):
+
+    form = Cities(request.POST or None)
+    if form.is_valid():
+        do_x() # custom logic here
+        return redirect('index')
+    return render(request, template_name, {'form': form})
 '''
