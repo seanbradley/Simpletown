d02710544f6f83ea1ed76e757d4379fd23050bdc
seanbradley <sean@blogblimp.com>
2013-08-18 17:58:00 -0700

    Change request method in form to GET.

--- cars/forms.py
+++ cars/forms.py
@@ -1,16 +1,28 @@
+import re
+
 from django import forms
+from django.core import validators
+
 from cars.models import Car
+
+#from django.core.validators import RegexValidatorimport re
+#namespace_regex = re.compile(r'^[a-z\-]+$') alias = models.CharField( max_length = 100, unique = True, validators=[RegexValidator(regex=namespace_regex)] )
+
+#code = forms.RegexField(regex=r'^\d{2}[-]\d{3}', max_length=6, widget=forms.TextInput(attrs=attrs_dict), label="Postal code")
+
+#class RegexValidator([regex=None, message=None, code=None])
 
 
 class CarForm(forms.Form):
-    vsn = forms.CharField(max_length=12, label='VSN')
+    #vsn = forms.CharField(max_length=12, label='VSN')
+    vsn = forms.RegexField(regex=r'^[a-zA-Z\*]{6}[0-9\*]{6}$', error_messages = {'invalid': "Please enter a valid VSN."}, label='VSN')
 
     def clean(self):
         data = self.cleaned_data.get('vsn')
-        if Car.objects.filter(vsn=data).exists():  #add custom validation here
+        if Car.objects.filter(vsn=data).exists():
             return data
         else:
-            raise forms.ValidationError("Please enter a valid VSN.")
+            raise forms.ValidationError("That VSN does not exist.")
 
     class Meta:
         model = Car
